26b90272465b433a7e5ca56b3b5c023ada81be2e
Kishore Ch <kishu.559@gmail.com>
2023-10-05 17:26:01 +0530

    final submit

--- star-rating/src/App.js
+++ star-rating/src/App.js
@@ -1,20 +1,34 @@
 import "./App.css";
 import { FaRegStar, FaStar } from "react-icons/fa";
+import { useState } from "react";
 
 function App() {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
   return (
     <div className="App">
       <h1>Star Rating</h1>
-      <FaRegStar color="yellow" size={30} />
       {[...Array(5)].map((star, index) => {
         const currentRating = index + 1;
         return (
           <label>
-            <input type="radio" name="rating" value={currentRating} />
-            <FaStar size={30} />
+            <input
+              type="radio"
+              name="rating"
+              value={currentRating}
+              onClick={() => setRating(currentRating)}
+            />
+            <FaStar
+              className="star"
+              color={currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+              onMouseEnter={() => setHover(currentRating)}
+              onMouseLeave={() => setHover(null)}
+              size={30}
+            />
           </label>
         );
       })}
+      <p>Total Rating is: {rating}</p>
     </div>
   );
 }
